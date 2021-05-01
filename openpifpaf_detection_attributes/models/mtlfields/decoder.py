39d71b5229c566bfdccfac99c298b9c649884945
LucMichels <36677739+LucMichels@users.noreply.github.com>
2021-05-01 17:54:24 +0200

    bug fixed: was no inversion for ground truth in metrics: now evaluating both lambdas0 and detection to check if everything is back to normal

--- openpifpaf_detection_attributes/models/mtlfields/decoder.py
+++ openpifpaf_detection_attributes/models/mtlfields/decoder.py
@@ -151,7 +151,7 @@ class InstanceDecoder(openpifpaf.decoder.decoder.Decoder):
         LOG.info('predictions %d, %.3fs',
                   len(predictions), time.perf_counter()-start)
 
-        return [predictions[-1]]
+        return predictions
 
 
     def cluster_vote(self, field, cluster, meta, conf_field):
