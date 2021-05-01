b19a4e291c1ebd32c6652925a40e91e2e8d3f27c
LucMichels <36677739+LucMichels@users.noreply.github.com>
2021-05-01 09:28:59 +0200

    training a new working metric

--- openpifpaf_detection_attributes/datasets/metrics.py
+++ openpifpaf_detection_attributes/datasets/metrics.py
@@ -222,7 +222,6 @@ class InstanceDetection(openpifpaf.metric.base.Base):
                 for i in range(len(ious)):
                     print(type(ious[i]))
                     print(i, ious[i])
-                1/0
                 # Classify predictions as True Positives or False Positives
                 if match is not None:
                     if (
