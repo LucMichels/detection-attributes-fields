dd22c8c2cf0448ad227d37a6a140f5cb575bf01e
LucMichels <36677739+LucMichels@users.noreply.github.com>
2021-05-02 22:12:17 +0200

    remove pifpaf predictions from predicts

--- openpifpaf_detection_attributes/datasets/metrics.py
+++ openpifpaf_detection_attributes/datasets/metrics.py
@@ -116,8 +116,8 @@ class InstanceDetection(openpifpaf.metric.base.Base):
         # Store predictions for writing to file
         pred_data = []
         for pred in predictions:
-            print("accumulate", type(pred))
-            pred_data.append(pred.json_data())
+            if not isinstance(pred, openpifpaf.annotation.Annotation):
+                pred_data.append(pred.json_data())
         self.predictions[image_meta['image_id']] = pred_data
 
         # Compute metrics
