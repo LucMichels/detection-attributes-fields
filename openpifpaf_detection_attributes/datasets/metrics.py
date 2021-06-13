1aafa2dc12db0c77bc62435450b76906486781db
LucMichels <36677739+LucMichels@users.noreply.github.com>
2021-06-13 19:19:56 +0200

    erased pred by mistake

--- openpifpaf_detection_attributes/datasets/metrics.py
+++ openpifpaf_detection_attributes/datasets/metrics.py
@@ -517,8 +517,8 @@ class InstanceHazikDetection(openpifpaf.metric.base.Base):
                             
                             # check if True positive
                             preds = [pred.attributes["is_not_crossing_reg"], pred.attributes["is_crossing_reg"]]
-                            pred = argmax(preds) if att == "is_crossing" else 1 - argmax(preds)
-                            tp = pred == gt.attributes[att] and gt.attributes[att] == 1
+                            p = argmax(preds) if att == "is_crossing" else 1 - argmax(preds)
+                            tp = p == gt.attributes[att] and gt.attributes[att] == 1
                             tp = int(tp)
                             self.cros_stats[att]['tp'].append(tp)
                             self.cros_stats[att]['fp'].append(1-tp)
@@ -655,7 +655,7 @@ class ClassificationHazik(openpifpaf.metric.base.Base):
 
                     # get prediction
                     preds = [match.attributes["is_not_crossing_reg"], match.attributes["is_crossing_reg"]]
-                    pred = argmax(preds) if att == "is_crossing" else 1 - argmax(preds)
+                    p = argmax(preds) if att == "is_crossing" else 1 - argmax(preds)
                     score = softmax(preds/sum(preds))[1] if att == "is_crossing" else softmax(preds/sum(preds))[0]
                     self.cros_stats[att]['score'].append(score)
 
@@ -663,18 +663,18 @@ class ClassificationHazik(openpifpaf.metric.base.Base):
                     self.cros_stats[att]['pred'].append(pred)
                     self.cros_stats[att]['true'].append(truth) 
 
-                    tp = int(pred == truth and truth == 1)
+                    tp = int(p == truth and truth == 1)
                     self.cros_stats[att]['tp'].append(tp)
                     self.cros_stats[att]['fp'].append(1-tp)    
 
                 else:
                     # Default to predicting not crossing
-                    pred = 0 if att == "is_crossing" else 1
+                    p = 0 if att == "is_crossing" else 1
                     self.cros_stats[att]['score'].append(0.0)
                     self.cros_stats[att]['pred'].append(pred)
                     self.cros_stats[att]['true'].append(gt.attributes[att])      
                     truth = gt.attributes[att]
-                    tp = int(pred == truth and truth == 1)
+                    tp = int(p == truth and truth == 1)
                     self.cros_stats[att]['tp'].append(tp)
                     self.cros_stats[att]['fp'].append(1-tp)     
 
