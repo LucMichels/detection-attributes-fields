532dcfa8f138960bc363e21be87ef63470e07d74
LucMichels <36677739+LucMichels@users.noreply.github.com>
2021-06-16 11:20:12 +0200

    test

--- openpifpaf_detection_attributes/datasets/metrics.py
+++ openpifpaf_detection_attributes/datasets/metrics.py
@@ -534,7 +534,8 @@ class InstanceHazikDetection(openpifpaf.metric.base.Base):
 
             for pred in predictions:
                 preds = [pred.attributes["is_not_crossing_reg"], pred.attributes["is_crossing_reg"]]
-                pred.attributes['score'] = pred.attributes['confidence'] * softmax(preds/sum(preds))[0 if att == "is_not_crossing" else 1]
+                p = argmax(preds) if att == "is_crossing" else 1 - argmax(preds)
+                pred.attributes['score'] = pred.attributes['confidence'] * p
             # Rank predictions based on confidences
             ranked_preds = sorted(predictions, key=lambda x:x.attributes['score'], reverse=True)
 
@@ -569,21 +570,21 @@ class InstanceHazikDetection(openpifpaf.metric.base.Base):
                         if not gt_match[match.id]:
                             
                             # check if True positive
-                            preds = [pred.attributes["is_not_crossing_reg"], pred.attributes["is_crossing_reg"]]
-                            p = argmax(preds) if att == "is_crossing" else 1 - argmax(preds)
-                            tp = p == gt.attributes[att] and gt.attributes[att] == 1 # redundant now that we used if (gt.id in gt_match) ?
-                            tp = int(tp)
+                            # preds = [pred.attributes["is_not_crossing_reg"], pred.attributes["is_crossing_reg"]]
+                            # p = argmax(preds) if att == "is_crossing" else 1 - argmax(preds)
+                            # tp = p == gt.attributes[att] and gt.attributes[att] == 1 # redundant now that we used if (gt.id in gt_match) ?
+                            tp = int(1)
                             self.cros_stats[att]['tp'].append(tp)
                             self.cros_stats[att]['fp'].append(1-tp)
 
                             gt_match[match.id] = True
                             score = pred.attributes['confidence'] * softmax(preds/sum(preds))[0 if att == "is_not_crossing" else 1]
-                            self.cros_stats[att]['score'].append(score)
+                            self.cros_stats[att]['score'].append(pred.attributes['score'])
                         else:
                             duplicates+=1
                             # False positive (multiple detections)
                             score = pred.attributes['confidence'] * softmax(preds/sum(preds))[0 if att == "is_not_crossing" else 1]
-                            self.cros_stats[att]['score'].append(score)
+                            self.cros_stats[att]['score'].append(pred.attributes['score'])
                             self.cros_stats[att]['tp'].append(0)
                             self.cros_stats[att]['fp'].append(1)
 
